c3432cd39809f00134b3bc6321f90a0f5075a854
ahuesoa <ahuesoa@uoc.edu>
2018-10-11 16:53:41 +0200

    Conexión con base de datos exitosa con e-mail y contraseña// También se ha añadido el google-services.json

--- app/src/main/java/com/pla1/cifo/ahuesoa/pac1/BookListActivity.java
+++ app/src/main/java/com/pla1/cifo/ahuesoa/pac1/BookListActivity.java
@@ -9,11 +9,23 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.pla1.cifo.ahuesoa.pac1.dummy.DummyContent;
 
 import java.util.List;
@@ -38,6 +50,67 @@ public class BookListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+
+
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+
+        //Usuario y clave prueba
+
+        String email="albertohueso@gmail.com";
+        String password="holahola";
+
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>()
+        {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task)
+            {
+                if (task.isSuccessful())
+                {
+                    Log.w("LoginSuccess", "signInWithEmail:success");
+
+                }
+
+                else
+                {
+                    Log.w("failureLogin", "signInWithEmail:failure", task.getException());
+
+                }
+                if (!task.isSuccessful())
+                {
+                   Log.w("noSuccessfullogin","no success");
+                }
+
+            }
+        });
+
+
+
+        FirebaseDatabase database=FirebaseDatabase.getInstance();
+        DatabaseReference myRef=database.getReference();
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                /*
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("prova1", "Value is: " + value);*/
+
+                Log.d("Libros", dataSnapshot.toString());
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("prova2", "Failed to read value.", error.toException());
+            }
+        });
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
