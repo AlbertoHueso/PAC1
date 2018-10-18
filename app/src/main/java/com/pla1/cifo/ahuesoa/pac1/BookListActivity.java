8f01e942d7cf02900c6ec4c5fa22a2557ef8a0d1
ahuesoa <ahuesoa@uoc.edu>
2018-10-18 11:23:35 +0200

    Cambiado a cardview

--- app/src/main/java/com/pla1/cifo/ahuesoa/pac1/BookListActivity.java
+++ app/src/main/java/com/pla1/cifo/ahuesoa/pac1/BookListActivity.java
@@ -14,13 +14,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
+
 import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.EmailAuthProvider;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
