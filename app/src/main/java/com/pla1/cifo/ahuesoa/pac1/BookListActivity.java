9d49a6c73a84a524b17b6dc9fd0ef19d14946e0f
ahuesoa <ahuesoa@uoc.edu>
2018-10-27 12:15:28 +0200

    Se ha pasado la clase ConexionDatabase a BookListActivity, por encapsulacion

--- app/src/main/java/com/pla1/cifo/ahuesoa/pac1/BookListActivity.java
+++ app/src/main/java/com/pla1/cifo/ahuesoa/pac1/BookListActivity.java
@@ -19,10 +19,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
 import com.pla1.cifo.ahuesoa.pac1.dummy.DummyContent;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import model.BookContent;
@@ -101,7 +106,7 @@ public class BookListActivity extends AppCompatActivity {
             co.start();
 
             //Conectamos a la base de datos
-            ConexionDatabase conexionDatabase=new ConexionDatabase(getApplicationContext(),this,books);
+            ConexionDatabase conexionDatabase=new ConexionDatabase();
             conexionDatabase.start();
         }
 
@@ -352,5 +357,108 @@ public class BookListActivity extends AppCompatActivity {
               toast.show();
           }
         }
+    }
+
+    /**
+     * Clase que realiza la conexión a la base de datos de Firebase
+     * Si es cancelado carga en la variable books los libros de la base de datos local
+     * Si lo consigue, carga en la variable books los libros de la base de datos en Firebase
+     * Extiende Thread para que sea un hilo propio y así poder retrasarlo independientemente del hilo de la autentificación
+     */
+    private class ConexionDatabase extends Thread{
+
+        /**
+         * Base de datos de Firebase
+         */
+        private FirebaseDatabase database = FirebaseDatabase.getInstance();
+        /**
+         * Referencia de base de datos, con el path books, es decir, la ruta desde la base de datos hasta los diferentes libros
+         */
+        private DatabaseReference myRef = database.getReference("books");
+
+
+
+        /**
+         * Constructor
+         *
+         */
+        public ConexionDatabase() {
+
+        }
+
+        @Override
+        /**
+         * En primer lugar ejecuta una espera de 1500ms
+         * Después abre un escuchador para eventos de cambio de datos
+         * Si se reciben nuevos datos los carga en books
+         * Si hay algún problema carga desde la base de datos local
+         */
+        public void run() {
+            try {
+                Thread.sleep(1500);
+                Log.d("loginEspera","espera");
+            }catch (InterruptedException e){
+                System.err.print(e.getMessage());
+                e.printStackTrace();
+            }
+
+            // Leemos de la base de datos
+            //Abrimos escuchador de eventos
+            myRef.addValueEventListener(new ValueEventListener() {
+
+
+                //Caso conexión a la base de datos exitosa
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+
+
+                    //Cargamos los libros leídos de la base de datos de Firebase en books
+                    try {
+
+                        GenericTypeIndicator<BookContent<BookItem>> t = new GenericTypeIndicator<BookContent<BookItem>>() {
+                        };
+
+                        //Cargamos los libros del snapshot en un ArrayList
+                        //No podemos hacerlo en un BookContent directamente porque el casting no funciona
+                        ArrayList<BookItem> booksArray = dataSnapshot.getValue(t);
+
+
+                        //Guardamos el booksArray en el BookContent
+                        books = Funciones.toBookContent(booksArray);
+
+
+                        //Cargamos los libros en la actividad
+                        loadItemList(books);
+
+                        //Mensaje informativo
+                        Toast toast = Toast.makeText(getApplicationContext(), "CONNECTED TO EXTERNAL DATABASE\nREADING FROM FIREBASE DATABASE", Toast.LENGTH_LONG);
+                        toast.show();
+
+                        //Actualizamos los identificadores, ya que desde Firebase vienen todos a 0
+                        updateIdentificatorsAndSaveNewItemsLocally(books);
+
+                        //Hay algún problema
+                    } catch (Exception e) {
+                        //Cargamos datosLocales
+                        showLocalData();
+                        System.err.println(e.getMessage());
+                        e.printStackTrace();
+                    }
+                }
+
+
+                //Conexión a la base de datos fallida
+                @Override
+                public void onCancelled(DatabaseError error) {
+                    // Error al leer el valor
+                    Log.e("lecturaError", "Failed to read value.", error.toException());
+                    //Cargamos datosLocales
+                    showLocalData();
+
+                }
+            });
+        }
+
+
     }
 }
